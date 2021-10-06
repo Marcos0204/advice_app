fe721238cf3fbb75a34cbdd1750b6ff13f7a3995
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-10-05 23:56:11 -0500

    creating-component-app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 
-const App = () => {
+import './App.css';
+
+class App extends React.Component {
+  state = {
+    advice: '',
+  }
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios.get('https://api.adviceslip.com/advice')
+      .then((response) => {
+        const { advice } = response.data.slip;
+
+        this.setState({ advice });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  render() {
     return (
-        <div>
-            <h1>App</h1>
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{this.state.advice}</h1>
+          <button className="button" onClick={this.fetchAdvice}>
+            <span>GIVE ME ADVICE!</span>
+          </button>
         </div>
-    )
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
